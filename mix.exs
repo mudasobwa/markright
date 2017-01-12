4d5e86d40e7e5dde991f1258971949aa306a0dac
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-01-12 17:32:33 +0100

    README.

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Markright.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
@@ -33,5 +35,30 @@ defmodule Markright.Mixfile do
       {:credo, "~> 0.5", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
     ]
+  end
+
+  defp description do
+    """
+    **The extended, configurable markdown-like syntax parser, that produces an AST.**
+
+    Out of the box is supports the full set of `markdown`, plus some extensions.
+    The user of this library might easily extend the functionality with her own
+    markup definition and a bit of elixir code to handle parsing.
+
+    There is no one single call to `Regex` used. The whole parsing is done solely
+    on pattern matching the input binary.
+
+    The AST produced is understandable by [`XmlBuilder`](https://github.com/joshnuss/xml_builder).
+    """
+  end
+
+  defp package do
+    [
+     name: :markright,
+     files: ["lib", "mix.exs", "README.md", "LICENSE"],
+     maintainers: ["Aleksei Matiushkin"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/mudasobwa/markright",
+              "Docs" => "https://hexdocs.pm/markright"}]
   end
 end
