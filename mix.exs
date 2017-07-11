b99cf9b2f85f9a1370972974461813247a0f7faf
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2017-07-11 15:01:25 +0200

    :paperclip: Version bump.

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Markright.Mixfile do
   def project do
     [
       app: @app,
-      version: "0.5.1",
+      version: "0.5.2",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
