30d6fb84260f429a99127d7f58403a3ab571eae2
Guilherme Castro <guilherme.torres.castro@gmail.com>
2021-04-26 13:04:29 -0300

    Use custom parse with mod name different from tag

--- lib/markright/utils.ex
+++ lib/markright/utils.ex
@@ -77,7 +77,13 @@ defmodule Markright.Utils do
   ##############################################################################
 
   @spec to_parser_module(atom(), list()) :: atom()
-  def to_parser_module(atom, opts \\ []),
+  def to_parser_module(atom, opts \\ [])
+
+  def to_parser_module(_atom, [parser: mod] = opts) do
+    if Code.ensure_loaded?(mod), do: mod, else: opts[:fallback]
+  end
+
+  def to_parser_module(atom, opts),
     do: to_module(Markright.Parsers, atom, opts)
 
   @spec to_finalizer_module(atom(), list()) :: atom()
